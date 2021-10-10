3cabd9cc06cb4195c9b15819dd78c69fd22ccc27
Michael <fugu137@gmail.com>
2021-10-10 20:29:37 +1100

    Adjusted security configuration to avoid back-end redirects (e.g., after login or logout)

--- src/main/java/net/mjduncan/watchlist/server/configuration/SecurityConfiguration.java
+++ src/main/java/net/mjduncan/watchlist/server/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package net.mjduncan.watchlist.server.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,13 +13,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${watchlist.cors.allowed.origins}")
+    private String allowedOrigins;
 
     @Autowired
     private UserAccountDetailsService userAccountDetailsService;
@@ -29,6 +42,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        configuration.setAllowedMethods(singletonList("*"));
+        configuration.setAllowedHeaders(singletonList("*"));
+        configuration.setExposedHeaders(singletonList("Location"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userAccountDetailsService);
@@ -37,20 +65,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
+                .exceptionHandling()
+                    .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorised"))
                     .and()
                 .authorizeRequests()
-                    .mvcMatchers(HttpMethod.POST, "/accounts").permitAll()
+                    .mvcMatchers(HttpMethod.POST, "/accounts").not().authenticated()
                     .anyRequest().authenticated()
-                .and()
+                    .and()
+                .cors()
+                    .and()
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .and()
                 .formLogin()
-                    .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
-                    .defaultSuccessUrl("/movies", true)
+//                    .loginPage("/login")
+                    .successHandler(savedRequestAwareAuthenticationSuccessHandler())
                     .and()
                 .logout()
-                    .logoutSuccessUrl("/login")
-                    .and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                    .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+    }
 
+    private SavedRequestAwareAuthenticationSuccessHandler savedRequestAwareAuthenticationSuccessHandler() {
+        SavedRequestAwareAuthenticationSuccessHandler handler = new SavedRequestAwareAuthenticationSuccessHandler();
+        handler.setRedirectStrategy((request, response, url) -> {});
+
+        return handler;
     }
 }
