780bbdbb5c85da81eb1c3d5d6bdd2c38b94411c4
Michael <fugu137@gmail.com>
2021-12-01 20:24:43 +1100

    Added functionality to find movie in omdb and add it as a user movie.

--- src/main/resources/schema-local.sql
+++ src/main/resources/schema-local.sql
@@ -1,25 +1,17 @@
---Drop Tables--
-DROP TABLE IF EXISTS accounts_movies;
-
-DROP TABLE IF EXISTS accounts;
-
-DROP TABLE IF EXISTS movies;
-
-
 --Create Tables--
-CREATE TABLE accounts (
+CREATE TABLE IF NOT EXISTS accounts (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(55) UNIQUE NOT NULL,
     password VARCHAR(155) NOT NULL,
     role VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE movies (
+CREATE TABLE IF NOT EXISTS movies (
     imdb_id VARCHAR(10) PRIMARY KEY,
     title VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE accounts_movies (
+CREATE TABLE IF NOT EXISTS accounts_movies (
     account_id BIGINT REFERENCES accounts(id),
     movie_id VARCHAR(10) REFERENCES movies(imdb_id),
     PRIMARY KEY(account_id, movie_id)
